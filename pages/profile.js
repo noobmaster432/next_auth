e2070b0cf6021fb15b60eb46ac45b3a112fa08c0
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-02-07 01:22:16 +0530

    adding next auth

--- pages/profile.js
+++ pages/profile.js
@@ -1,30 +1,40 @@
-import Link from "next/link"
-import {getSession} from "next-auth/react"
+import Link from "next/link";
+import { getSession } from "next-auth/react";
+import Head from "next/head";
 
-export default function profile(){
-    return (
-        <section className="container mx-auto text-center">
-            <h3 className="text-4xl font-bold">Profile Page</h3>
-            <Link href={'/'}>Home Page</Link>
-        </section>
-    )
+export default function profile() {
+  return (
+    <div>
+        <Head>
+            <title>Profile</title>
+        </Head>
+      <section className="container mx-auto text-center py-20">
+        <h3 className="text-4xl font-bold mb-10">Profile Page</h3>
+        <Link href={"/"}>
+          <span className="px-10 py-2 bg-slate-100 hover:bg-slate-200 rounded-lg">
+            Home Page
+          </span>
+        </Link>
+      </section>
+    </div>
+  );
 }
 
-export async function getServerSideProps({req}){
-    const session = await getSession({req})
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
 
-    if(!session){
-        return {
-            redirect: {
-                destination: '/login',
-                permanent: false
-            }
-        }
-    }
-    // authorize user return session
+  if (!session) {
     return {
-        props: {
-            session
-        }
-    }
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  // authorize user return session
+  return {
+    props: {
+      session,
+    },
+  };
 }
